a25c0820c53f2adfc22ef9a753a16c6b4376d26b
Niltonsf <nilton.schumacher.filho@gmail.com>
2022-06-13 15:57:57 -0300

    first commit

---
+++ src/Experience/World/World.js
@@ -0,0 +1,26 @@
+import * as THREE from 'three';
+import Experience from "../Experience";
+import Environment from "./Environment";
+import Floor from './Floor';
+import Fox from './Fox';
+
+export default class World {
+	constructor() {
+		this.experience = new Experience();
+
+		this.scene = this.experience.scene;
+		this.resources = this.experience.resources;	
+
+		// Wait for resources
+		this.resources.on('ready', () => {
+			// Class Instance
+			this.floor = new Floor();
+			this.fox = new Fox();
+			this.environment = new Environment();
+		});
+	}
+
+	update() {
+		if (this.fox) this.fox.update();
+	}
+}
