cdb98d8cffa0851d5ddb42400e83235adc34fb00
Niltonsf <nilton.schumacher.filho@gmail.com>
2022-12-07 10:25:49 -0300

    feat: creating the basic experience and making react three fiber and drei work

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import Experience from "./experience/Experience";
+import { Canvas } from "@react-three/fiber";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Canvas
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 2000,
+        position: [-3, 1.5, 4],
+      }}
+    >
+      <Experience />
+    </Canvas>
   </React.StrictMode>
 );
